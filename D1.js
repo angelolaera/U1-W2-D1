64979bb7b4452331ba575fc44839267aed313126
angelolaera <angelo.laera.1995@gmail.com>
2024-05-09 18:21:11 +0200

    Update D1.js

--- D1.js
+++ D1.js
@@ -63,7 +63,7 @@ console.log(4-x)
 const name1="john"
 const name2="John"
 console.log(name1===name2)
-const name3=name2.toLowerCase()
 
-console.log(name1===name3)
+
+console.log(name1===name2.toLowerCase())
 
